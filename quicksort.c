f8565e5dca521b21dac3561dd15064b766d0a4de
George Lucas Lazarim <georgelucas1402@gmail.com>
2026-06-10 22:02:45 -0300

    Partition function

---
+++ quicksort.c
@@ -0,0 +1,21 @@
+#include "quicksort.h"
+
+int partition (int array[], int f, int l) {
+    int pivot = array[l];
+    int i = f - 1;
+
+    for (int j = f; j < l; j++) {
+        if (array[j] < pivot) {
+            i++;
+            int aux = array[j];
+            array[j] = array[i];
+            array[i] = aux;
+        }
+    }
+
+    int aux = array[i + 1];
+    array[i + 1] = array[l];
+    array[l] = aux;
+
+    return i + 1;
+}
---
+++ quicksort.h
@@ -0,0 +1,6 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+int partition (int array[], int low, int high);
+
+#endif
